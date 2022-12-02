da82909af09a7f8b49cdbdfb766a775256e4d04c
msuecvb <msuecvb@au.com>
2022-12-02 17:07:11 +0900

    var0.1.0

--- index.php
+++ index.php
@@ -9,4 +9,16 @@ echo "今日の運勢は大吉です。";
 echo "今日の運勢は吉です。";
 }
 
+$sum += 1;
+echo $sum
+
+$link = mysqli_connect('test-sql-php.database.windows.net', 'testuser', 'Yume$2020', 'test-sql-php-database');
+
+// 接続状況をチェックします
+if (mysqli_connect_errno()) {
+    die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+} else {
+    echo "データベースの接続に成功しました。\n";
+}
+
 ?>
