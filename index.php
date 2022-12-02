bf0a78411b88b222bf048c1773c45715c7b4d261
msuecvb <msuecvb@au.com>
2022-12-02 18:27:27 +0900

    エラー回避処理実装

--- index.php
+++ index.php
@@ -9,6 +9,11 @@ echo "今日の運勢は大吉です。";
 echo "今日の運勢は吉です。";
 }
 
-
+try {
+    // エラーが出るかもしれない処理
+    $link = mysqli_connect('test-sql-php.database.windows.net', 'testuser', 'Yume$2020', 'test-sql-php-database');
+} catch (Exception $e) {
+    echo "エラー".$e->getMessage();
+}
 
 ?>
