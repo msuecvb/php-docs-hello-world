73ec6485c0d1f4ed44f8f15b59cf441f43a2e555
msuecvb <msuecvb@au.com>
2022-12-02 18:47:48 +0900

    コメントアウト

--- index.php
+++ index.php
@@ -17,11 +17,13 @@ echo "今日の運勢は大吉です。";
 echo "今日の運勢は吉です。";
 }
 
+/*
+
 try {
     // エラーが出るかもしれない処理
     $link = mysqli_connect('test-sql-php.database.windows.net', 'testuser', 'Yume$2020', 'test-sql-php-database');
 } catch (Exception $e) {
     echo "エラー".$e->getMessage();
 }
-
+*/
 ?>
