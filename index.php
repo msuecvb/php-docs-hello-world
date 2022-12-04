9cbfc26390dff15db07a19057af836e9c29a1319
msuecvb <msuecvb@au.com>
2022-12-05 02:05:03 +0900

    サーバー修正

--- index.php
+++ index.php
@@ -18,6 +18,27 @@ echo "今日の運勢は大吉です。";
 echo "今日の運勢は吉です。";
 }
 
+ $serverName = "https://smplephp.azurewebsites.net"; // update me
+    $connectionOptions = array(
+        "Database" => "test-sql-php.database.windows.net", // update me
+        "Uid" => "testuser", // update me
+        "PWD" => "Yume$2020" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+         FROM [SalesLT].[ProductCategory] pc
+         JOIN [SalesLT].[Product] p
+         ON pc.productcategoryid = p.productcategoryid";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+
 /*
 
 try {
